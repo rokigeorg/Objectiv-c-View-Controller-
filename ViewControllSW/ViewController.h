2f3e5b8549ed6344a8967ba6a0c0f04fa506af9a
Georg Rokita <georgrokita@georg-daniel-rokitas-macbook-pro.local>
2013-11-28 17:55:29 +0100

    Initial Commit

---
+++ ViewControllSW/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ViewControllSW
+//
+//  Created by Georg Rokita on 28.11.13.
+//  Copyright (c) 2013 Georg Rokita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ViewControllSW/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewControllSW
+//
+//  Created by Georg Rokita on 28.11.13.
+//  Copyright (c) 2013 Georg Rokita. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
